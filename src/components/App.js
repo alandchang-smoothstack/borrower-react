b6b24bb11bb1173c85d809c254b459ce6c0a6a73
Aland <aland.chang@smoothstack.com>
2020-03-18 18:41:14 -0400

    first commit

---
+++ src/components/App.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import { HashRouter, Switch, Route } from 'react-router-dom';
+import './App.css';
+
+function App() {
+  return (
+    <div>
+      <HashRouter>
+        <Switch>
+          <Route />
+        </Switch>
+      </HashRouter>
+    </div>
+  );
+}
+
+export default App;
