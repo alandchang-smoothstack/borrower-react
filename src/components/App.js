19e0028baff43b35a6d4428c37f10091a0fb3e4a
Achavez97 <achavz97@berkeley.edu>
2020-03-21 19:02:21 -0400

    Added home page

--- src/components/App.js
+++ src/components/App.js
@@ -5,6 +5,7 @@ import './App.css';
 import LoginContainer from './Login/LoginContainer'
 import CheckoutContainer from './Checkout/CheckoutContainer';
 import ReturnContainer from './Return/ReturnContainer';
+import Home from './Home';
 
 function App() {
   return (
@@ -12,6 +13,7 @@ function App() {
       <HashRouter>
         <Switch>
           <Route path='/login' component={LoginContainer} />
+          <Route path='/home' component={Home} />
           <Route path='/checkout' component={CheckoutContainer} />
           <Route path='/return' component={ReturnContainer} />
         </Switch>
